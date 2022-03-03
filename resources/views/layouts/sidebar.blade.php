6f79fa3675bf01d5244ecbb61ae685442250b6b4
EGYuda23 <egyyudanugraha@gmail.com>
2022-03-03 21:49:17 +0700

    add button logout

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -35,6 +35,12 @@
                     </li>
                 </ul>
             </li>            
+            <li class="sidebar-item">
+                <a href="/logout" class='sidebar-link'>
+                    <i class="bi bi-box-arrow-right"></i>
+                    <span>Logout</span>
+                </a>
+            </li>
         </ul>
     </div>
     <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
